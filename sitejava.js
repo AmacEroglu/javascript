12a39e6c9a5c90f30fbc3adfc45290e9c2a2b302
AmacEroglu <amacceroglu@gmail.com>
2022-06-22 02:27:16 +0300

    start

---
+++ sitejava.js
@@ -0,0 +1,34 @@
+
+// selamla("Amaç");
+// selamla("Sevgi");
+
+
+// function selamla(name) {
+
+//     console.log("merhaba "+ name);
+//     alert('merhaba '+ name);
+//     document.getElementById("mesaj").innerText = "merhaba "+ name;
+// }
+
+// selamla("Amaç");
+
+// function getNameSurname() {
+//     let name=document.getElementById("name").value;
+//     let surname=document.getElementById("surname").value;
+//     let nameSurname= name+ " " + surname;
+//     return nameSurname;
+// }
+
+// function selamla() {
+//     let result=getNameSurname();
+//     console.log("merhaba "+ result);
+//     alert('merhaba '+ result);
+//     document.getElementById("mesaj").innerText = "merhaba "+ result;
+// }
+
+
+// <!-- ödev -->
+
+    
+
+// <!-- ödev -->
